bf3ea5ed860a0ed5cd8b4f168369c6be4ba886c4
paramagicdev <konnor5456@gmail.com>
2020-03-08 20:52:02 -0400

    converting to tsx

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -5,12 +5,19 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import Helmet from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title }) {
+interface Props {
+  description: string;
+  lang: string;
+  meta: Array<object>;
+  title: string;
+}
+
+function SEO({ description, lang, meta, title }): React.FC<Props> {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -23,9 +30,9 @@ function SEO({ description, lang, meta, title }) {
         }
       }
     `
-  )
+  );
 
-  const metaDescription = description || site.siteMetadata.description
+  const metaDescription = description || site.siteMetadata.description;
 
   return (
     <Helmet
@@ -69,13 +76,7 @@ function SEO({ description, lang, meta, title }) {
         },
       ].concat(meta)}
     />
-  )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
+  );
 }
 
 SEO.propTypes = {
@@ -83,6 +84,12 @@ SEO.propTypes = {
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
-}
+};
 
-export default SEO
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+};
+
+export default SEO;
