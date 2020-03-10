743b1ea4809159334d82cca002495f9a26b03def
paramagicdev <konnor5456@gmail.com>
2020-03-10 17:56:50 -0400

    Add module alias imports

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+
+import path from "path";
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        $: path.resolve(__dirname, "src/"),
+        $components: path.resolve(__dirname, "src/components"),
+        $styles: path.resolve(__dirname, "src/styles"),
+      },
+    },
+  });
+};
