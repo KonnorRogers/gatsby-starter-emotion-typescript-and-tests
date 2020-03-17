38ed5a4ebc388dc3e08655848dd381a1a2e1f611
paramagicdev <konnor5456@gmail.com>
2020-03-17 11:57:49 -0400

    need to fix issues with first / last-child selectors

---
+++ __tests__/components/seo/seo.test.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { render } from "@react/testing-library";
+import SEO from "~components/seo";
+
+describe("SEO component", () => {
+  test.skip("", () => {});
+});
