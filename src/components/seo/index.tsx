e34ef72562c1f831d64f79efd173b4bc4357beb8
paramagicdev <konnor5456@gmail.com>
2020-03-18 09:58:28 -0400

    fixing React-helmet structure

--- src/components/seo/index.tsx
+++ src/components/seo/index.tsx
@@ -9,31 +9,68 @@ import React from "react";
 import Helmet from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-export interface Props {
-  title: string;
-  description?: string;
-  lang?: string;
-  meta?: Array<object>;
-}
-
 export interface SiteProps {
-  site: {
-    siteMetadata: {
-      title?: string;
-      description?: string;
-      author?: string;
-    };
+  siteMetadata: {
+    title: string;
+    description: string;
+    author: string;
   };
 }
 
-export function PureSEO({
+type MetaProps = JSX.IntrinsicElements["meta"];
+
+export interface Props {
+  title?: string;
+  description?: string;
+  lang?: string;
+  meta?: MetaProps[];
+  site: SiteProps;
+}
+
+export const PureSEO = ({
   site,
   description,
   lang,
   meta,
   title,
-}: Props & SiteProps): JSX.Element {
+}: Props): JSX.Element => {
   const metaDescription = description || site.siteMetadata.description;
+  const metaData: MetaProps[] = meta || [];
+
+  const metaTags: MetaProps[] = [
+    {
+      name: `description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: site.siteMetadata.author,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: metaDescription,
+    },
+  ];
 
   return (
     <Helmet
@@ -42,46 +79,13 @@ export function PureSEO({
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta || [])}
+      meta={metaTags.concat(metaData)}
     />
   );
-}
+};
 
-function SEO(attrs: Props): JSX.Element {
-  const { site } = useStaticQuery(
+function SEO({ title, ...attrs }: HelmetProps): JSX.Element {
+  const site: SiteProps = useStaticQuery(
     graphql`
       query {
         site {
@@ -93,9 +97,9 @@ function SEO(attrs: Props): JSX.Element {
         }
       }
     `
-  );
+  ).site;
 
-  return <PureSEO site={site} {...attrs} />;
+  return <PureSEO title={title} site={site} {...attrs} />;
 }
 
 export default SEO;
